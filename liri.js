9994d0bc21d950addd4132a0f5ad836472213099
jay <jaydanasara@yahoo.com>
2018-11-12 02:27:03 -0500

    update2

--- liri.js
+++ liri.js
@@ -18,13 +18,17 @@ if (input1 === `concert-this`) {
 
 
   request(queryBUrl, function (error, response, body) {
+    space="\n";
 
     if (!error && response.statusCode === 200) {
       for (i = 0; i < JSON.parse(body).length; i++) {
-        console.log("The artist will be at this Venue " + JSON.parse(body)[i].venue.name);
-        console.log("State " + JSON.parse(body)[i].venue.region);
-        console.log("City " + JSON.parse(body)[i].venue.city);
-        console.log("Date and Time " + JSON.parse(body)[i].datetime);
+        output1="The artist will be at this Venue " + JSON.parse(body)[i].venue.name+space+
+        "State " + JSON.parse(body)[i].venue.region+space+ "City " + JSON.parse(body)[i].venue.city+space+
+        "Date and Time " + JSON.parse(body)[i].datetime+space+space;
+        console.log(output1);
+        fs.appendFile("logtxt",output1, function (err){
+          if (err) throw err;
+        })
       }
 
 
@@ -37,28 +41,12 @@ if (input1 === `concert-this`) {
 
 }
 
-else if (input1==="spotify-this-song" && input2===undefined){
-input2= "the sign (ace of base)"
-  var spotify = new Spotify(keys.spotify);
-  var space = "\n";
-  spotify.search({ type: 'track', query: input2 }, function (err, data) {
-    if (err) {
-      console.log('Error occurred: ' + err);
-      return;
-    }
-    else if (!err) {
-      output = space + "================= DATA HERE ==================" +
-        space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
-        space + "Song Name: " + "'" + input2.toUpperCase() + "'" +
-        space + "Album Name: " + data.tracks.items[0].album.name +
-        space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
-      console.log(output);
-    }
-  
+
 
 
 else if
 (input1 === `spotify-this-song`) {
+  spot()
 
   var spotify = new Spotify(keys.spotify);
   var space = "\n";
@@ -74,6 +62,9 @@ else if
         space + "Album Name: " + data.tracks.items[0].album.name +
         space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
       console.log(output);
+      fs.appendFile("logtxt",output,function (err){
+        if (err) throw err;
+      })
     }
 
 
@@ -89,20 +80,25 @@ else if
 else if
 (input1 === `movie-this`) {
 
-
+j();
   var queryUrl = "http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy";
 
-  console.log(queryUrl);
+  
 
   request(queryUrl, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log("The movie's title is: " + JSON.parse(body).Title);
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-      console.log("The movie's country is: " + JSON.parse(body).Country);
-      console.log("The movie's language is: " + JSON.parse(body).Language);
-      console.log("The movie's plot is: " + JSON.parse(body).Plot);
-      console.log("The movie's actors are : " + JSON.parse(body).Anctors);
+    space="\n";
+    if (!error && response.statusCode === 200) { 
+      output2=  "The movie's title is: " + JSON.parse(body).Title+space+
+      "Release Year: " + JSON.parse(body).Year+space+
+      "The movie's rating is: " + JSON.parse(body).imdbRating+space+
+      "The movie's country is: " + JSON.parse(body).Country+space+
+      "The movie's language is: " + JSON.parse(body).Language+space+
+      "The movie's plot is: " + JSON.parse(body).Plot+space+
+      "The movie's actors are : " + JSON.parse(body).Actors+space+space;
+      console.log(output2)
+      fs.appendFile("logtxt", output2,function (err){
+        if (err) throw err;
+      })
     }
   });
 
@@ -112,8 +108,24 @@ else if
 
 
 }
+ 
+
 // // else if
 // // (input1 === `do-what-it-says`) {
 
 
 // // }
+ function j (){
+  if (input1==="movie-this"&&input2===undefined){
+    input2="mr nobody";
+    console.log(" If you haven't watched Mr. Nobody, then you should <http://www.imdb.com/title/tt0485947/>  It's on Netflix!")
+  
+  };
+}
+
+function spot(){
+  if (input1==="spotify-this-song"&&input2===undefined){
+
+    input2= "the sign(ace of base)";
+  };
+}
